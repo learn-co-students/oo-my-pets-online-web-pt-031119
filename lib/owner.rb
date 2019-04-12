6226da8040e4646970d5dca0273581a76b31c8ab
Leeya Davis <leeya463@gmail.com>
2019-04-12 04:23:37 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,7 +26,50 @@ class Owner
     "I am a #{@species}."
   end
 
-  def buy_fish
+  def buy_fish (name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
 
+  def buy_cat (name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog (name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.map do |pet_type, pet|
+      pet.map {|a| a.mood = "nervous"}
+    end
+    @pets.clear
+  end
+
+  def list_pets
+    f_count = @pets[:fishes].size
+    c_count = @pets[:cats].size
+    d_count = @pets[:dogs].size
+    "I have #{f_count} fish, #{d_count} dog(s), and #{c_count} cat(s)."
   end
 end
